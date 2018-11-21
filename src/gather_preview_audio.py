70504857136d1f25f4cd2c13a9aed86ac45f0dd4
Alexander Keijser <alexanderkeijser73@gmail.com>
2018-11-21 01:11:05 +0100

    fixes and first audio previews

--- src/gather_preview_audio.py
+++ src/gather_preview_audio.py
@@ -4,8 +4,13 @@ from get_preview_url import get_trackid_from_text_search
 import hdf5_utils
 import hdf5_getters as GETTERS
 
+total_count = 0
+unavailable_count = 0
+unfound_count = 0
+
+
 for dirpath, dirs, files in os.walk('../data/MillionSongSubset/data'):
-    for filename in fnmatch.filter(files, '*.h5'):
+    for i, filename in enumerate(fnmatch.filter(files, '*.h5')):
         h5path = os.path.join(dirpath, filename)
         # open h5 song, get all we know about the song
         h5 = hdf5_utils.open_h5_file_read(h5path)
@@ -18,7 +23,14 @@ for dirpath, dirs, files in os.walk('../data/MillionSongSubset/data'):
         res = get_trackid_from_text_search(track_name, artistname=artist_name)
         if res is None:
             print('Did not find track using artist name and track title')
+            unfound_count +=1
         else:
             name, preview_url = res
-            print(res)
+            if preview_url is None:
+                unavailable_count += 1
             # print(preview_url)
+
+        total_count +=1
+        if i %100 == 0:
+            print("Total tracks: {} Unfound tracks: {} Unavailable tracks {}".format(total_count, unfound_count,
+                                                                                     unavailable_count))
