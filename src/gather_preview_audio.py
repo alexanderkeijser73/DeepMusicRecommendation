30ebd1b0ee66b0937dcc27a722e7e74ab8202227
alexanderkeijser73 <alexanderkeijser73@gmail.com>
2018-11-30 17:01:15 +0100

    discovered that i don't need entire MSD to get track information!

--- src/gather_preview_audio.py
+++ src/gather_preview_audio.py
@@ -5,6 +5,8 @@ import hdf5_utils
 import hdf5_getters as GETTERS
 import re
 import wget
+import signal
+import sys
 
 total_count = 0
 unavailable_count = 0
@@ -12,44 +14,56 @@ unfound_count = 0
 
 OUTDIR = '../../data/MillionSongSubset/audio'
 
+h5path = '../../data/msd_summary_file.h5'
+
+
 if not os.path.isdir(OUTDIR):
     os.mkdir(OUTDIR)
 
-for dirpath, dirs, files in os.walk('../../data/MillionSongSubset/data'):
-    for i, filename in enumerate(fnmatch.filter(files, '*.h5')):
-        h5path = os.path.join(dirpath, filename)
-        # open h5 song, get all we know about the song
-        h5 = hdf5_utils.open_h5_file_read(h5path)
-        artist_name = GETTERS.get_artist_name(h5).decode('utf-8')
-        track_name = GETTERS.get_title(h5).decode('utf-8')
-        h5.close()
+h5 = hdf5_utils.open_h5_file_read(h5path)
+num_songs = GETTERS.get_num_songs(h5)
 
-        artist_name_re = re.sub(' *[\[\(]*featuring*.*[\]\)]*', '', artist_name, flags=re.IGNORECASE)
-        track_name_re = re.sub(' *[\[\(]+.*[\]\)]+', '', track_name)
 
-        # print('Searching for track: ', artist_name, ' - ', track_name)
-        # search by artist name + track title
-        res = get_trackid_from_text_search(track_name_re, artistname=artist_name_re)
-        if res is None:
-            # print('Did not find track using artist name and track title')
-                unfound_count +=1
-                with open('../../data/MillionSongSubset/unfound_tracks.txt', 'a+') as f:
-                    f.write('\t'.join([os.path.splitext(filename)[0], artist_name, track_name]) + '\n')
-        else:
-            name, preview_url, id = res
+for i in range(num_songs):
+    artist_name = GETTERS.get_artist_name(h5, songidx=i).decode('utf-8')
+    track_name = GETTERS.get_title(h5, songidx=i).decode('utf-8')
+    track_id = GETTERS.get_track_id(h5, songidx=i).decode('utf-8')
+
+    out_path = os.path.join(OUTDIR, os.path.splitext(track_id)[0]) + '.mp3'
+    if os.path.exists(out_path):
+        continue
+
+    track_name = re.sub('_', '', track_name)
+    artist_name_re = re.sub(' *([;_/&,*]|(feat))+.*', '', artist_name)
+    artist_name_re = re.sub(' *[\[\(]*feat*.*[\]\)]*', '', artist_name_re, flags=re.IGNORECASE)
+    track_name_re = re.sub(' *[\[\(]+.*[\]\)]+', '', track_name)
+    artist_name_re = re.sub(' *[\[\(]*featuring*.*[\]\)]*', '', artist_name_re, flags=re.IGNORECASE)
+    track_name_re = re.sub(' *[\[\(]+.*[\]\)]+', '', track_name_re)
+
+    # print('Searching for track: ', artist_name, ' - ', track_name)
+    # search by artist name + track title
+    res = get_trackid_from_text_search(track_name_re, artistname=artist_name_re)
+    if res is None:
+        # print('Did not find track using artist name and track title')
+            unfound_count +=1
+            with open('../../data/MillionSongSubset/unfound_tracks.txt', 'a+') as f:
+                f.write('\t'.join([os.path.splitext(track_id)[0], artist_name, track_name]) + '\n')
+    else:
+        name, preview_url, id = res
+        if preview_url is None:
+            preview_url = get_preview_from_trackid(id)
             if preview_url is None:
-                preview_url = get_preview_from_trackid(id)
-                if preview_url is None:
-                    unavailable_count += 1
-                    with open('../../data/MillionSongSubset/unavailable_tracks.txt', 'a+') as f:
-                        f.write('\t'.join([os.path.splitext(filename)[0], artist_name, track_name]) + '\n')
-            if preview_url is not None:
-                out_path = os.path.join(OUTDIR, os.path.splitext(filename)[0]) + '.mp3'
-                if not os.path.exists(out_path):
-                    wget.download(preview_url, out=out_path)
-            # print(preview_url)
+                unavailable_count += 1
+                with open('../../data/MillionSongSubset/unavailable_tracks.txt', 'a+') as f:
+                    f.write('\t'.join([os.path.splitext(track_id)[0], artist_name, track_name]) + '\n')
+        if preview_url is not None:
+            out_path = os.path.join(OUTDIR, os.path.splitext(track_id)[0]) + '.mp3'
+            if not os.path.exists(out_path):
+                wget.download(preview_url, out=out_path)
+        # print(preview_url)
 
-        total_count +=1
-        if i % 100 == 0:
-            print("Total tracks: {} Unfound tracks: {} Unavailable tracks {}".format(total_count, unfound_count,
-                                                                                     unavailable_count))
+    total_count +=1
+    if i % 100 == 0:
+        print("Total tracks: {} Unfound tracks: {} Unavailable tracks {}".format(i, unfound_count,
+                                                                                 unavailable_count))
+h5.close()
